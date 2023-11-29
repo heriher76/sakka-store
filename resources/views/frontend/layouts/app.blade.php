a32e7208fee92e30fc715ecf4a4bbdd38a2f5b2b
Heri Hermawan <herihermawan@Heris-MacBook-Air.local>
2023-11-30 01:57:00 +0700

    whatsapp

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -166,6 +166,25 @@
         }
 
         .pac-container { z-index: 100000; }
+
+        .float{
+            position:fixed;
+            width:60px;
+            height:60px;
+            bottom:40px;
+            right:40px;
+            background-color:#25d366;
+            color:#FFF;
+            border-radius:50px;
+            text-align:center;
+            font-size:30px;
+            box-shadow: 2px 2px 3px #999;
+            z-index:100;
+        }
+
+        .my-float{
+            margin-top:16px;
+        }
     </style>
 
 @if (get_setting('google_analytics') == 1)
@@ -296,6 +315,11 @@
             </div>
         </div>
     </div>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <a href="{{get_setting('whatsapp_link')}}" class="float" target="_blank">
+        <i class="fa fa-whatsapp my-float"></i>
+    </a>
 
     @yield('modal')
 
